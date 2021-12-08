63b7bf050f9ced829fa89fbde218a6f78326783c
bchavez <bartolomechavezcampano2@gmail.com>
2021-12-08 16:23:08 -0300

    adding a custom button

--- aam_flutter_forms/lib/aam_flutter_forms.dart
+++ aam_flutter_forms/lib/aam_flutter_forms.dart
@@ -9,19 +9,30 @@ class Calculator {
   int addOne(int value) => value + 1;
 }
 
-class AamButton extends ElevatedButton {
-  AamButton()
-      : super(
-          child: Text('Aam Button'),
-          onPressed: () {
-            print('Aam Button pressed');
-          },
-        );
+class AamButton extends StatelessWidget {
+  final String content;
+  final double height;
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return ButtonElement()
-  //     ..text = text
-  //     ..onClick.listen((_) => onPressed());
-  // }
+  AamButton({required this.content, this.height = 44});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width / 1.4,
+      height: height,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Center(
+        child: Text(
+          content,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
 }
